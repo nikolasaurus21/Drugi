43359d27ad42d1642ba8c24dedbfdd21c1b3d084
nikol <nikol@DESKTOP-PKFUAKN>
2022-07-08 10:11:00 +0200

    Add project files.

---
+++ Drugi/Program.cs
@@ -0,0 +1,41 @@
+﻿
+public interface IStartCar
+{
+     public void Start();
+}
+
+public class StartCar : IStartCar
+{
+    public void Start()
+    {
+        Console.WriteLine("paljenje auta");
+    }
+} 
+
+public class RunCar
+{
+    private IStartCar _car;
+    public RunCar(IStartCar car)
+    {
+        _car = car;
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("auto je upaljeno mozete krenuti");
+    }
+}
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var startovanje = new StartCar();
+        var pokretanje = new RunCar(startovanje);
+        startovanje.Start();
+        await Task.Delay(2500);
+        pokretanje.Run();
+
+
+    }
+}
